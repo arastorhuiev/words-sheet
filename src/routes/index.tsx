c4e1f171aa4582c70a3c04317039f08067a16b29
Andrii Rastorhuiev <arastorhuiev@gmail.com>
2026-02-07 22:14:48 +0200

    add biome and saas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,7 +9,9 @@ export default function Home() {
         Visit{" "}
         <a href="https://start.solidjs.com" target="_blank">
           start.solidjs.com
+
         </a>{" "}
+
         to learn how to build SolidStart apps.
       </p>
     </main>
